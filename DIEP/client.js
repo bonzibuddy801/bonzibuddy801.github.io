d72362b8a4cec060ff55d968d2bb6ff3594a763b
bonzibuddy801 <bitemee801@gmail.com>
2018-04-18 20:25:54 -0500

    Update client.js

--- DIEP/client.js
+++ DIEP/client.js
@@ -92,7 +92,7 @@ function param(name) {
 		/\+/g, "%20")) || null;
 }
 
-const connectIP = defaults(param("ip"), "http://pewter-zebra.glitch.me/");
+const connectIP = defaults(param("ip"), "https://pewter-zebra.glitch.me/");
 
 let socket = io.connect(connectIP, {
 	reconnect: false,
